cbe8b21fa0cadd0de92e203a164dc670f1fa5db8
Tristan Hucher <tristan.hucher@etu.utc.fr>
2023-11-20 14:19:16 +0100

    ajout interaction entre agent

--- cmd/astar/main.go
+++ cmd/astar/main.go
@@ -15,14 +15,118 @@ import (
 const SCREEN_WIDTH = 700
 const SCREEN_HEIGHT = 700
 
+
+func signedAcos(x float64) float64 {
+	unsignedAcos := math.Acos(x)
+	if x >= 0 {
+		return unsignedAcos
+	} else {
+		return -unsignedAcos
+	}
+}
+
+
 type Agent struct {
-	x, y, vx, vy, gx, gy, speed, reactivity float64
+	x, y, vx, vy, gx, gy, speed, reactivity float64 // je pense qu'on peut retirer les vx, vy, gx, gy, tx, ty des attributs 
 	tx, ty                                  float64
 	controllable                            bool
 	path            						[]*astar.Node
 	currentWayPoint 						int
 	goal 									*astar.Node
-	start									*astar.Node			
+	start									*astar.Node	
+	channelAgent							chan []*Agent
+	channelMur								chan [][2]int
+}
+
+
+func (a *Agent)calculePosition ()error{
+
+	var wayPoint *astar.Node
+
+	// move agent towards current waypoint at a speed of 2px per frame
+	if a.currentWayPoint < len(a.path) {
+		wayPoint = a.path[a.currentWayPoint]
+		a.vx = float64(wayPoint.Pos.X*7) + 3.5 - a.x
+		a.vy = float64(wayPoint.Pos.Y*7) + 3.5 - a.y
+		vNorm := math.Sqrt(a.vx*a.vx + a.vy*a.vy)
+
+		a.vx = a.vx / vNorm
+		a.vy = a.vy / vNorm
+
+		//prise en compte des murs
+		var vectx, vecty, normeEucli, reactionMurX, reactionMurY  float64
+		var listeMur [][2]int
+		listeMur = <- a.channelMur
+		for _, mur := range listeMur {
+			vectx = float64(mur[0])*7 + 3.5 - a.x
+			vecty = float64(mur[1])*7 + 3.5 - a.y
+			normeEucli = math.Sqrt((float64(mur[0]*7)+3.5-a.x)*(float64(mur[0]*7)+3.5-a.x) + (float64(mur[1]*7)+3.5-a.y)*(float64(mur[1]*7)+3.5-a.y))
+			vectx = vectx / normeEucli
+			vecty = vecty / normeEucli
+
+			reactionMurX = vectx * (3 * math.Exp(-normeEucli/2)) * 10
+			reactionMurY = vecty * (3 * math.Exp(-normeEucli/2)) * 10
+
+			a.vx -= reactionMurX
+			a.vy -= reactionMurY
+		}
+
+
+		// change velocity to avoid other agents following moussaïd 2009
+		var listeAgent []*Agent
+		listeAgent = <- a.channelAgent
+		for _,otherAgent := range listeAgent{
+			lambda := 2.0
+			A := 4.5
+			gamma := 0.35
+			n := 2.0
+			np := 3.0
+			factor := 0.15
+
+			//pour ne pas recalculer la distance on pourrait la passé dans le channel via un dictionnaire ? A discuter
+			dist := math.Sqrt((a.x*factor-otherAgent.x*factor)*(a.x*factor-otherAgent.x*factor) + (a.y*factor-otherAgent.y*factor)*(a.y*factor-otherAgent.y*factor))
+			
+			ex := (otherAgent.x*factor - a.x*factor) / dist
+			ey := (otherAgent.y*factor - a.y*factor) / dist
+			Dx := lambda*(a.vx*factor-otherAgent.vx*factor) + ex
+			Dy := lambda*(a.vy*factor-otherAgent.vy*factor) + ey
+			DNorm := math.Sqrt(Dx*Dx + Dy*Dy)
+			tx := Dx / DNorm
+			ty := Dy / DNorm
+			// nx, ny is the normal vector to tx,ty pointing to the left
+			nx := ty
+			ny := -tx
+			a.tx = nx
+			a.ty = ny
+			//theta := math.Acos(math.Min(math.Max(ex*tx+ey*ty, -1), 1)) / (2 * math.Pi) * 360
+			theta := signedAcos(math.Min(math.Max(ex*tx+ey*ty, -1), 1))
+			B := gamma * DNorm
+			addedToVX := -A * math.Exp(-dist/B) * (math.Exp(-math.Pow(np*B*theta, 2))*tx + math.Exp(-math.Pow(n*B*theta, 2))*nx) / factor
+			addedToVY := -A * math.Exp(-dist/B) * (math.Exp(-math.Pow(np*B*theta, 2))*ty + math.Exp(-math.Pow(n*B*theta, 2))*ny) / factor
+			// safeguard against too big values
+			if addedToVX > 10 {
+				addedToVX = 0
+			}
+			if addedToVY > 10 {
+				addedToVY = 0
+			}
+			a.vx += addedToVX
+			a.vy += addedToVY
+
+		}
+		a.x += a.vx
+		a.y += a.vy
+
+		//passage à l'étape d'après :
+		if math.Sqrt((float64(wayPoint.Pos.X*7)+3.5-a.x)*(float64(wayPoint.Pos.X*7)+3.5-a.x)+(float64(wayPoint.Pos.Y*7)+3.5-a.y)*(float64(wayPoint.Pos.Y*7)+3.5-a.y)) < 2 {
+			a.currentWayPoint++
+		}
+	} else {
+		<- a.channelMur
+		<- a.channelAgent
+
+	}
+	return nil
 }
 
 type Simulation struct {
@@ -33,6 +137,8 @@ type Simulation struct {
 }
 
 func (s *Simulation) Update() error {
+
+	//initialization
 	if s.m == nil {
 		// find route to goal
 		s.m = astar.NewMap(100, 100)
@@ -46,6 +152,8 @@ func (s *Simulation) Update() error {
 
 		for i:=0;i<s.nAgents; i++{
 		
+			s.agents[i].channelAgent = make(chan []*Agent,1)
+			s.agents[i].channelMur = make(chan [][2]int,1)
 
 			//s.m.SetCell(astar.Position{X: int(s.agentX / 7), Y: int(s.agentY / 7)}, astar.EmptyCell)
 			s.agents[i].start = &astar.Node{Pos: astar.Position{X: int(s.agents[i].x / 7), Y: int(s.agents[i].y / 7)}}
@@ -64,46 +172,37 @@ func (s *Simulation) Update() error {
 
 	}
 
-	var wayPoint *astar.Node
-	
+	//update
 	for i:=0;i<s.nAgents; i++{
 
-		// move agent towards current waypoint at a speed of 2px per frame
-		if s.agents[i].currentWayPoint < len(s.agents[i].path) {
-			wayPoint = s.agents[i].path[s.agents[i].currentWayPoint]
-			s.agents[i].vx = float64(wayPoint.Pos.X*7) + 3.5 - s.agents[i].x
-			s.agents[i].vy = float64(wayPoint.Pos.Y*7) + 3.5 - s.agents[i].y
-			vNorm := math.Sqrt(s.agents[i].vx*s.agents[i].vx + s.agents[i].vy*s.agents[i].vy)
+		listeAgent := make([]*Agent, 0, s.nAgents)
+		listeMur := make([][2]int, 0)
 
-			s.agents[i].vx = s.agents[i].vx / vNorm
-			s.agents[i].vy = s.agents[i].vy / vNorm
-
-			var vectx, vecty, normeEucli, reactionMurX, reactionMurY  float64
-
-			for _, mur := range s.walls {
-				vectx = float64(mur[0])*7 + 3.5 - s.agents[i].x
-				vecty = float64(mur[1])*7 + 3.5 - s.agents[i].y
-				normeEucli = math.Sqrt((float64(mur[0]*7)+3.5-s.agents[i].x)*(float64(mur[0]*7)+3.5-s.agents[i].x) + (float64(mur[1]*7)+3.5-s.agents[i].y)*(float64(mur[1]*7)+3.5-s.agents[i].y))
-				if normeEucli > 50 {
-					continue
-				}
-				vectx = vectx / normeEucli
-				vecty = vecty / normeEucli
-
-				reactionMurX = vectx * (3 * math.Exp(-normeEucli/2)) * 10
-				reactionMurY = vecty * (3 * math.Exp(-normeEucli/2)) * 10
-
-				s.agents[i].vx -= reactionMurX
-				s.agents[i].vy -= reactionMurY
-			}
-
-			s.agents[i].x += s.agents[i].vx
-			s.agents[i].y += s.agents[i].vy
-
-			if math.Sqrt((float64(wayPoint.Pos.X*7)+3.5-s.agents[i].x)*(float64(wayPoint.Pos.X*7)+3.5-s.agents[i].x)+(float64(wayPoint.Pos.Y*7)+3.5-s.agents[i].y)*(float64(wayPoint.Pos.Y*7)+3.5-s.agents[i].y)) < 2 {
-				s.agents[i].currentWayPoint++
+		//loading agent'channel with walls around them 
+		for _,mur := range s.walls{
+			normeEucli := math.Sqrt((float64(mur[0]*7)+3.5-s.agents[i].x)*(float64(mur[0]*7)+3.5-s.agents[i].x) + (float64(mur[1]*7)+3.5-s.agents[i].y)*(float64(mur[1]*7)+3.5-s.agents[i].y))
+			if normeEucli < 50 {
+				listeMur = append(listeMur, mur)
 			}
 		}
+		s.agents[i].channelMur <- listeMur		
+
+		//loading agent'channel with other agents around them 
+		for _,otherAgent := range s.agents{
+			if otherAgent == s.agents[i] {
+				continue
+			}
+			factor := 0.15
+			dist := math.Sqrt((s.agents[i].x*factor-otherAgent.x*factor)*(s.agents[i].x*factor-otherAgent.x*factor) + (s.agents[i].y*factor-otherAgent.y*factor)*(s.agents[i].y*factor-otherAgent.y*factor))
+			if dist < 10 {
+				listeAgent = append(listeAgent, otherAgent)
+			}
+		}
+		s.agents[i].channelAgent <- listeAgent
+
+		go s.agents[i].calculePosition()
+
+		
 	}
 	return nil
 }
@@ -164,10 +263,10 @@ func main() {
 		return
 	}
 
-	nAgents := 5
+	nAgents := 10
 	agents := make([]*Agent,0,5)
 	for i:=0;i<nAgents; i++{
-		agents = append(agents,&Agent{x: float64(70*i), y: float64(70)})
+		agents = append(agents,&Agent{x: float64(70+35*i), y: float64(70)})
 	}
 
 	sim := Simulation{
